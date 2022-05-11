9b3e1d08518ccf6b9c94bd47b4421df65c31259d
nimeshikagit <nimemobile@gmail.com>
2022-05-11 16:51:44 +0530

    project_ structure, routing , login-page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { FeaturesComponent } from './component/features/features.component';
+import { PricingComponent } from './component/pricing/pricing.component';
+import { BlogComponent } from './component/blog/blog.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'features',
+    component: FeaturesComponent,
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'log',
+    component: LoginComponent,
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
