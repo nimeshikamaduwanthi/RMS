64b43537f69247e1d5c57b43af09357e021dd513
nimeshikagit <nimemobile@gmail.com>
2022-05-19 13:12:24 +0530

    completed blog and set home page img

--- src/app/component/blog/blog.component.ts
+++ src/app/component/blog/blog.component.ts
@@ -1,15 +1,21 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
+  providers: [DatePipe],
 })
 export class BlogComponent implements OnInit {
+  currentDate = new Date().toString();
+  currentMonth = new Date().toString();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private datePipe: DatePipe) {
+    this.currentDate = this.datePipe.transform(this.currentDate, 'dd') ?? '';
+    this.currentMonth = this.datePipe.transform(this.currentMonth, 'MMM') ?? '';
+    console.log(this.datePipe.transform(this.currentDate, 'MMM') ?? '');
   }
 
+  ngOnInit(): void {}
 }
